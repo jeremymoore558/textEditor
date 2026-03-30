3cfb14f62cef150d1c74c3e50d46e618a9c140ce
jeremymoore558 <jeremymoore558@gmail.com>
2026-03-29 19:39:56 -0500

    Implemented character deletion in insert mode.

--- src/input_handler.c
+++ src/input_handler.c
@@ -10,20 +10,32 @@ void write_char_to_buffer(char c, DocBuffer* doc_buf){
 	doc_buf->buffer[doc_buf->cursor.left] = c;
 }
 
+void delete_char_from_buffer(DocBuffer* doc_buf){
+	doc_buf->buffer[doc_buf->cursor.left] = '\0';
+	doc_buf->cursor.left -= 1;
+}
+
 void insert_mode(DocBuffer* doc_buf){
 	//TODO Finish
 	char c;
 	while (1){
 		c = '\0';
 		read(STDIN_FILENO, &c, 1);
-
-		if (c == 27){
-			break;
-		}else if (c == 13){
-			write_char_to_buffer('\n', doc_buf);
-		}else{
-			write_char_to_buffer(c, doc_buf);
+		if (c == 27){break;}
+		switch (c){
+			case '\0':
+				break;
+			case 13:
+				write_char_to_buffer('\n', doc_buf);
+				break;	
+			case 127:
+				delete_char_from_buffer(doc_buf);
+				break;
+			default:
+				write_char_to_buffer(c, doc_buf);
+				break;
 		}
+
 		clear_screen();
 		print_buffer(doc_buf);
 	}
