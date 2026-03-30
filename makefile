e51339fe8d6d21a00f71ac618045c9f7a9862612
jeremymoore558 <jeremymoore558@gmail.com>
2026-03-29 20:06:26 -0500

    Implemented vim-style o command. Moves to end of line, inserts newline character.

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CC = gcc
 CFLAGS = -Wall
 
-all: main.o document_buffer.o input_handler.o a
+all: main.o document_buffer.o input_handler.o insert_mode.o a
 
 main.o: ./src/main.c ./src/main.h
 	$(CC) -c ./src/main.c -o ./build/main.o
@@ -12,5 +12,8 @@ document_buffer.o: ./src/document_buffer.c ./src/document_buffer.h
 input_handler.o: ./src/input_handler.c ./src/input_handler.h
 	$(CC) -c ./src/input_handler.c -o ./build/input_handler.o
 
-a: ./build/main.o ./build/document_buffer.o ./build/input_handler.o
-	$(CC) -o ./bin/a ./build/main.o ./build/document_buffer.o ./build/input_handler.o
+insert_mode.o: ./src/insert_mode.c ./src/insert_mode.h
+	$(CC) -c ./src/insert_mode.c -o ./build/insert_mode.o
+
+a: ./build/main.o ./build/document_buffer.o ./build/input_handler.o ./build/insert_mode.o
+	$(CC) -o ./bin/a ./build/main.o ./build/document_buffer.o ./build/input_handler.o ./build/insert_mode.o
--- src/input_handler.c
+++ src/input_handler.c
@@ -1,97 +1,75 @@
 #include <termios.h>
 #include <unistd.h>
 #include "document_buffer.h"
-#include "main.h"
+#include "insert_mode.h"
 
-void write_char_to_buffer(char c, DocBuffer* doc_buf){
-	if (c == '\0'){return;}
-
+void move_cursor_down(DocBuffer* doc_buf){
 	doc_buf->cursor.left += 1;
-	doc_buf->buffer[doc_buf->cursor.left] = c;
-}
-
-void delete_char_from_buffer(DocBuffer* doc_buf){
-	doc_buf->buffer[doc_buf->cursor.left] = '\0';
-	doc_buf->cursor.left -= 1;
-}
-
-void insert_mode(DocBuffer* doc_buf){
-	//TODO Finish
-	char c;
-	while (1){
-		c = '\0';
-		read(STDIN_FILENO, &c, 1);
-		if (c == 27){break;}
-		switch (c){
-			case '\0':
-				break;
-			case 13:
-				write_char_to_buffer('\n', doc_buf);
-				break;	
-			case 127:
-				delete_char_from_buffer(doc_buf);
-				break;
-			default:
-				write_char_to_buffer(c, doc_buf);
-				break;
-		}
-
-		clear_screen();
-		print_buffer(doc_buf);
+	doc_buf->cursor.right += 1;
+	doc_buf->buffer[doc_buf->cursor.left] = 
+		doc_buf->buffer[doc_buf->cursor.right];
+	doc_buf->buffer[doc_buf->cursor.right] = '\0';
+	while (doc_buf->buffer[doc_buf->cursor.left] != '\n' &&
+			doc_buf->cursor.right < GAP_BUFFER_SIZE){
+		doc_buf->cursor.left += 1;
+		doc_buf->cursor.right += 1;
+		doc_buf->buffer[doc_buf->cursor.left] = 
+			doc_buf->buffer[doc_buf->cursor.right];
+		doc_buf->buffer[doc_buf->cursor.right] = '\0';
 	}
 }
 
-void cursor_motion(char c, DocBuffer* doc_buf){
+void move_cursor_up(DocBuffer* doc_buf){
 	int new_line_counter;
+	new_line_counter = 0;
+	while (new_line_counter < 2 && doc_buf->cursor.left >= 0){
+		if (doc_buf->buffer[doc_buf->cursor.left] == '\n'){new_line_counter += 1;}
+		if (new_line_counter < 2){
+			doc_buf->buffer[doc_buf->cursor.right] = 
+				doc_buf->buffer[doc_buf->cursor.left];
+			doc_buf->buffer[doc_buf->cursor.left] = '\0';
+			doc_buf->cursor.left -= 1;
+			doc_buf->cursor.right -= 1;
+		}
+	}
+
+}
+
+void move_cursor_left(DocBuffer* doc_buf){
+	if (doc_buf->cursor.left >= 0){
+		doc_buf->buffer[doc_buf->cursor.right] = 
+			doc_buf->buffer[doc_buf->cursor.left];
+		doc_buf->buffer[doc_buf->cursor.left] = '\0';
+		doc_buf->cursor.left -= 1;
+		doc_buf->cursor.right -= 1;
+	}
+}
+
+void move_cursor_right(DocBuffer* doc_buf){
+	if (doc_buf->cursor.right < GAP_BUFFER_SIZE-1){
+		doc_buf->cursor.left += 1;
+		doc_buf->cursor.right += 1;
+		doc_buf->buffer[doc_buf->cursor.left] = 
+			doc_buf->buffer[doc_buf->cursor.right];
+		doc_buf->buffer[doc_buf->cursor.right] = '\0';
+	}
+}
+
+
+
+void cursor_motion(char c, DocBuffer* doc_buf){
 	switch (c){
 		case 'h':
-			if (doc_buf->cursor.left >= 0){
-				doc_buf->buffer[doc_buf->cursor.right] = 
-					doc_buf->buffer[doc_buf->cursor.left];
-				doc_buf->buffer[doc_buf->cursor.left] = '\0';
-				doc_buf->cursor.left -= 1;
-				doc_buf->cursor.right -= 1;
-			}
+			move_cursor_left(doc_buf);
 			break;
 		case 'l':
-			if (doc_buf->cursor.right < GAP_BUFFER_SIZE-1){
-				doc_buf->cursor.left += 1;
-				doc_buf->cursor.right += 1;
-				doc_buf->buffer[doc_buf->cursor.left] = 
-					doc_buf->buffer[doc_buf->cursor.right];
-				doc_buf->buffer[doc_buf->cursor.right] = '\0';
-			}
+			move_cursor_right(doc_buf);
 			break;
 		case 'j':
-			doc_buf->cursor.left += 1;
-			doc_buf->cursor.right += 1;
-			doc_buf->buffer[doc_buf->cursor.left] = 
-				doc_buf->buffer[doc_buf->cursor.right];
-			doc_buf->buffer[doc_buf->cursor.right] = '\0';
-			while (doc_buf->buffer[doc_buf->cursor.left] != '\n' &&
-					doc_buf->cursor.right < GAP_BUFFER_SIZE){
-				doc_buf->cursor.left += 1;
-				doc_buf->cursor.right += 1;
-				doc_buf->buffer[doc_buf->cursor.left] = 
-					doc_buf->buffer[doc_buf->cursor.right];
-				doc_buf->buffer[doc_buf->cursor.right] = '\0';
-			}
+			move_cursor_down(doc_buf);
 			break;
 		case 'k':
-			new_line_counter = 0;
-			while (new_line_counter < 2 && doc_buf->cursor.left >= 0){
-				if (doc_buf->buffer[doc_buf->cursor.left] == '\n'){new_line_counter += 1;}
-				if (new_line_counter < 2){
-					doc_buf->buffer[doc_buf->cursor.right] = 
-						doc_buf->buffer[doc_buf->cursor.left];
-					doc_buf->buffer[doc_buf->cursor.left] = '\0';
-					doc_buf->cursor.left -= 1;
-					doc_buf->cursor.right -= 1;
-				}
-			}
-			break;
-		case 'i':
-			insert_mode(doc_buf);
+			move_cursor_up(doc_buf);
 			break;
 	}		
 }
@@ -101,6 +79,12 @@ void handle_inputs(char c, DocBuffer* doc_buf){
 		case 'q':
 			return;
 		case 'i':
+			insert_mode(doc_buf);
+			return;
+		case 'o':
+			move_cursor_down(doc_buf);
+			move_cursor_left(doc_buf);
+			write_char_to_buffer('\n', doc_buf);
 			insert_mode(doc_buf);
 			return;
 		default:
